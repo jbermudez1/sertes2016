c444e28e2ad3403b5ba45ecb348f91f68dea0ceb
diego <diego@proyeccione.com>
2016-04-05 16:57:01 -0600

    configuración
    modifique la conexión de la base de datos.

--- public/lib/bootstrap/Config.php
+++ public/lib/bootstrap/Config.php
@@ -4,10 +4,10 @@ class Config
 {
 
     protected $config = [
-        'database' => 'sertes',
-        'host' => 'localhost',
-        'username' => 'root',
-        'password' => 'root'
+        'database' => 'proyecc4_sertes',
+        'host' => '174.136.30.161',
+        'username' => 'proyecc4_sertes',
+        'password' => 'Sertes123'
     ];
 
     public function __get($var)
